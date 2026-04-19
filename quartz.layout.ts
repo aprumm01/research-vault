051d84f2c4cedd15e0bf517052470efbbcc8d0f0
Adam Prumm <adam.prumm@gmail.com>
2026-04-18 21:37:52 -0400

    Hide tag nodes from graph view

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -41,7 +41,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
+    Component.Graph({ showTags: false }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
